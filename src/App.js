5b13961912505820c54bf75315b01084897fc42b
divyansh <divyansh@hubilo.com>
2021-12-30 03:39:36 +0530

    search and deploy

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, {Component} from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/SearchBox'
 class App extends Component{
   constructor(){
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField : ''
     };
   }
   componentDidMount(){
@@ -15,9 +17,15 @@ class App extends Component{
     .then(users=>this.setState({monsters : users}));
   }
   render(){
+    const {monsters, searchField } = this.state; // destructuring
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
     return(
       <div className="App">
-        <CardList monsters = {this.state.monsters}/>
+        <h1>Monsters Rolodex</h1>
+        <SearchBox placeholder ='search monster' handleChange = {e=> this.setState({searchField: e.target.value})} />
+        <CardList monsters = {filteredMonsters}/>
     </div>
     );
   }
