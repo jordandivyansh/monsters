f8d20b2f46ac0a1c16e0691b7f5055cb21eb8c45
Divyansh Kumar <divyanshkrit@gmail.com>
2022-03-05 03:12:49 +0530

    converting state components into functions using hooks

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import {CardList} from './components/card-list/card-list.component';
 import {SearchBox} from './components/search-box/SearchBox'
-class App extends Component{
-  constructor(){
-    super();
-    this.state = {
-      monsters: [],
-      searchField : ''
-    };
-  }
-  componentDidMount(){
+
+function App() {
+  const [monster, setMonster] = useState([]);
+  const [searchField, setSearchField] = useState('');
+  
+  useEffect(()=>{
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response=>response.json())
-    .then(users=>this.setState({monsters : users}));
-  }
-  render(){
-    const {monsters, searchField } = this.state; // destructuring
-    const filteredMonsters = monsters.filter(monster => 
-      monster.name.toLowerCase().includes(searchField.toLowerCase())
-      );
-    return(
-      <div className="App">
-        <h1>Monsters Rolodex</h1>
-        <SearchBox placeholder ='search monster' handleChange = {e=> this.setState({searchField: e.target.value})} />
-        <CardList monsters = {filteredMonsters}/>
-    </div>
-    );
+      .then(response=>response.json())
+      .then(users=>setMonster(users));
+  },[])
+
+  const onSearchChange = (event)=>{
+    setSearchField(event.target.value)
   }
 
+  const filteredMonsters = monster.filter(monster => {
+    return monster.name.toLowerCase().includes(searchField.toLowerCase())
+  })
+
+  return !monster.length ?
+  <h1> Loading... </h1>:
+  (
+    <div className="App">
+      <h1>Monsters Rolodex</h1>
+      <SearchBox placeholder ='search monster' searchChange = {onSearchChange} />
+      <CardList monsters = {filteredMonsters}/>
+  </div>
+  );
 }
 
 export default App;
